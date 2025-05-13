95c4f7552c1752098a900b902a41ec166f67dbd9
Levi S. <levisporto@gmail.com>
2025-05-13 19:02:24 -0300

    added images and made grid

---
+++ script.js
@@ -0,0 +1,19 @@
+let card1 = document.getElementsByClassName("div1");
+let card2 = document.getElementsByClassName(".div2");
+let card3 = document.getElementsByClassName(".div3");
+let card4 = document.getElementsByClassName(".div4");
+let card5 = document.getElementsByClassName(".div5");
+let card6 = document.getElementsByClassName(".div6");
+let card7 = document.getElementsByClassName(".div7");
+let card8 = document.getElementsByClassName(".div8");
+let card9 = document.getElementsByClassName(".div9");
+let card10 = document.getElementsByClassName(".div10");
+let card11 = document.getElementsByClassName(".div11");
+let card12 = document.getElementsByClassName(".div12");
+
+card1.onclick = hello
+
+function hello(){
+    alert("hello!");
+}
+
