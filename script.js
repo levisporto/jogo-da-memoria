d7c165fe32ae419d69854ea223f5df573f4193da
Levi S. <levisporto@gmail.com>
2025-05-13 20:24:54 -0300

    cards can turn now

--- script.js
+++ script.js
@@ -1,19 +1,80 @@
-let card1 = document.getElementsByClassName("div1");
-let card2 = document.getElementsByClassName(".div2");
-let card3 = document.getElementsByClassName(".div3");
-let card4 = document.getElementsByClassName(".div4");
-let card5 = document.getElementsByClassName(".div5");
-let card6 = document.getElementsByClassName(".div6");
-let card7 = document.getElementsByClassName(".div7");
-let card8 = document.getElementsByClassName(".div8");
-let card9 = document.getElementsByClassName(".div9");
-let card10 = document.getElementsByClassName(".div10");
-let card11 = document.getElementsByClassName(".div11");
-let card12 = document.getElementsByClassName(".div12");
+let card1 = document.getElementById("div1");
+let card2 = document.getElementById("div2");
+let card3 = document.getElementById("div3");
+let card4 = document.getElementById("div4");
+let card5 = document.getElementById("div5");
+let card6 = document.getElementById("div6");
+let card7 = document.getElementById("div7");
+let card8 = document.getElementById("div8");
+let card9 = document.getElementById("div9");
+let card10 = document.getElementById("div10");
+let card11 = document.getElementById("div11");
+let card12 = document.getElementById("div12");
+let card13 = document.getElementById("div13");
+let card14 = document.getElementById("div14");
+let card15 = document.getElementById("div15");
 
-card1.onclick = hello
 
-function hello(){
-    alert("hello!");
+function turn1(){
+    alert("cuzin")
+    card1.src ="img/1.jpg"
 }
+
+function turn2(){
+    alert("cuzin")
+    card2.src ="img/2.jpg"
+}
+function turn3(){
+    alert("cuzin")
+    card3.src ="img/3.jpg"
+}
+function turn4(){
+    alert("cuzin")
+    card4.src ="img/4.jpg"
+}
+function turn5(){
+    alert("cuzin")
+    card5.src ="img/5.jpg"
+}
+function turn6(){
+    alert("cuzin")
+    card6.src ="img/6.jpg"
+}
+function turn7(){
+    alert("cuzin")
+    card7.src ="img/7.jpg"
+}
+function turn8(){
+    alert("cuzin")
+    card7.src ="img/8.jpg"
+}
+function turn9(){
+    alert("cuzin")
+    card9.src ="img/9.jpg"
+}
+function turn10(){
+    alert("cuzin")
+    card10.src ="img/10.jpg"
+}
+function turn11(){
+    alert("cuzin")
+    card11.src ="img/11.jpg"
+}
+function turn12(){
+    alert("cuzin")
+    card12.src ="img/12.jpg"
+}
+function turn13(){
+    alert("cuzin")
+    card13.src ="img/13.jpg"
+}
+function turn14(){
+    alert("cuzin")
+    card14.src ="img/14.jpg"
+}
+function turn15(){
+    alert("cuzin")
+    card15.src ="img/15.jpg"
+}
+
 
